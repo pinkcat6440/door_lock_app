1539b5a66f64fc4521320c6c846c9490c3c36207
pinkcat6440 <pinkcat6440@gmail.com>
2025-07-23 16:31:18 +0900

    Add files via upload
    build.gradle.kts(Modul:ch20_firebase)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,78 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
+}
+android {
+    namespace = "com.example.ch20_firebase"
+    compileSdk = 36
 
-    id("com.google.gms.google-services") version "4.4.3" apply false
+    defaultConfig {
+        applicationId = "com.example.ch20_firebase"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+        multiDexEnabled = true
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+    }
+
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+
+    // 테스트
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    // MultiDex 라이브러리
+    implementation("androidx.multidex:multidex:2.0.1")
+
+    // Firebase (BOM을 사용하여 버전 관리)
+    implementation(platform("com.google.firebase:firebase-bom:33.16.0")) // 최신 BOM 버전 확인
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth-ktx") // Firebase 인증
+    implementation("com.google.firebase:firebase-firestore-ktx") // Firebase Firestore
+    implementation("com.google.firebase:firebase-storage-ktx") // Firebase Storage
+    implementation("com.google.firebase:firebase-messaging-ktx") // Firebase 메시징 서비스
+
+    // Google Sign-In 라이브러리 (Firebase Auth 사용 시 필요)
+    implementation("com.google.android.gms:play-services-auth:21.0.0") // 최신 버전 사용 권장
+
+    // OkHttp (Retrofit에 의해 사용됨, 로깅 인터셉터는 디버그용)
+    implementation ("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+
 
 }
